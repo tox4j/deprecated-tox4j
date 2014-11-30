3a86d3c82dcced75beb84b82cf2e8b6138575d55
Simon Levermann <slever@fb3.uni-bremen.de>
2014-12-01 00:18:54 +0100

    Progress on thread safety of tox4j

---
+++ src/main/java/im/tox/tox4j/exceptions/ToxKilledException.java
@@ -0,0 +1,12 @@
+package im.tox.tox4j.exceptions;
+
+/**
+ * Exception to be thrown when a method is invoked on a tox instance that has been closed
+ *
+ * @author Simon Levermann (sonOfRa)
+ */
+public class ToxKilledException extends RuntimeException {
+    public ToxKilledException(String message) {
+        super(message);
+    }
+}
