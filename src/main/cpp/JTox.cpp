a919baa4fd40fa005719dc543d9b76f5c8386cc2
Simon Levermann <simon@slevermann.de>
2014-12-02 15:01:23 +0100

    Clean up CMakeList.txt files
    Set CXX Flags instead of C Flags, properly delete java class files on
    make clean, fix compiler warnings of -Wall -Werror -pedantic

--- src/main/cpp/JTox.cpp
+++ src/main/cpp/JTox.cpp
@@ -31,8 +31,8 @@ static inline jint throw_tox_killed_exception(JNIEnv *env, char const *message) 
  * Do setup here. Caching of needed java method IDs etc should be done in this function. It is guaranteed to be called
  * when the library is loaded, and nothing else will be called before this function is called.
  */
-jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-
+jint JNI_OnLoad(JavaVM *, void *) {
+    return 0;
 }
 
 JNIEXPORT jint JNICALL Java_im_tox_tox4j_Tox4j_toxNew(JNIEnv *env, jobject, jboolean ipv6enabled, jboolean udpDisabled,
