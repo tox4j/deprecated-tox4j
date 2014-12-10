b41ff597b97faaef27a8b127c79c3b3aac9a4e8c
Anonymized <anonymous@example.com>
2014-12-10 03:58:33 +0000

    Moved SBRM classes to utility header.
    To reduce cognitive load when editing JTox.cpp.

--- src/main/cpp/JTox.cpp
+++ src/main/cpp/JTox.cpp
@@ -6,41 +6,8 @@
 #include <utility>
 
 #include "JTox.h"
+#include "jniutil.h"
 #include "events.pb.h"
-
-struct UTFChars {
-    UTFChars(JNIEnv *env, jstring string)
-    : env(env)
-    , string(string)
-    , chars(env->GetStringUTFChars(string, 0)) { }
-
-    UTFChars(UTFChars const &) = delete;
-    ~UTFChars() { env->ReleaseStringUTFChars(string, chars); }
-
-    operator char const *() const { return chars; }
-
-private:
-    JNIEnv *env;
-    jstring string;
-    char const *chars;
-};
-
-struct ByteArray {
-    ByteArray(JNIEnv *env, jbyteArray byteArray)
-    : env(env)
-    , byteArray(byteArray)
-    , bytes(env->GetByteArrayElements(byteArray, 0)) { }
-
-    ByteArray(ByteArray const &) = delete;
-    ~ByteArray() { env->ReleaseByteArrayElements(byteArray, bytes, JNI_ABORT); }
-
-    operator uint8_t const *() const { return (uint8_t *)bytes; }
-
-private:
-    JNIEnv *env;
-    jbyteArray byteArray;
-    jbyte *bytes;
-};
 
 struct ToxDeleter {
     void operator()(Tox *tox) {
