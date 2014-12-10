8a7a7535f4675051ea71ce0f582bf8f301902263
vko <viktor.kostov@softwerk.se>
2014-12-10 01:13:24 +0100

    Add group {message, action, title change} callbacks

---
+++ src/main/java/im/tox/tox4j/callbacks/GroupMessageCallback.java
@@ -0,0 +1,18 @@
+package im.tox.tox4j.callbacks;
+
+/**
+ * Callback for group messages
+ *
+ * @author Viktor Kostov (sk8ter)
+ */
+public interface GroupMessageCallback {
+
+    /**
+     * Method to be executed when a group message is received
+     *
+     * @param groupNumber groupNumber the message was sent by
+     * @param peerNumber
+     * @param message     the message. Generally, this should be UTF-8, but this is not guaranteed.
+     */
+    void execute(int groupNumber, int peerNumber, byte[] message);
+}
