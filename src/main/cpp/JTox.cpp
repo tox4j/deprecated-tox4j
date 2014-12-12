50e6ef64c052296b608d9605a5104292ab7c8b19
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-12-12 19:18:30 +0000

    Move common C++ code into a separate header.

--- src/main/cpp/JTox.cpp
+++ src/main/cpp/JTox.cpp
@@ -1,189 +1,4 @@
-#include <algorithm>
-#include <vector>
-#include <tox/tox.h>
-#include <mutex>
-#include <memory>
-#include <sstream>
-#include <stdexcept>
-#include <utility>
-
-#include "JTox.h"
-#include "jniutil.h"
-#include "events.pb.h"
-
-using tox4j::proto::ToxEvents;
-
-struct ToxDeleter {
-    void operator()(Tox *tox) {
-        tox_kill(tox);
-    }
-};
-
-class Tox4jStruct {
-    // Objects of this class can conceptionally have three states:
-    // - LIVE: A tox instance is alive and running.
-    // - DEAD: The object is empty, all pointers are nullptr.
-    // - COLLECTED: state is DEAD, and the object's instance_number is in instance_freelist.
-    bool live = true;
-
-public:
-    std::unique_ptr<Tox, ToxDeleter> tox;
-    std::unique_ptr<ToxEvents> events;
-    std::unique_ptr<std::mutex> mutex;
-
-    bool isLive() const { return live; }
-    bool isDead() const { return !live; }
-
-    Tox4jStruct(std::unique_ptr<Tox, ToxDeleter> &&tox,
-                std::unique_ptr<ToxEvents> &&events,
-                std::unique_ptr<std::mutex> &&mutex)
-    : tox(std::move(tox))
-    , events(std::move(events))
-    , mutex(std::move(mutex))
-    { }
-
-    // Move members from another object into this new one, then set the old one to the DEAD state.
-    Tox4jStruct(Tox4jStruct &&rhs)
-    : tox(std::move(rhs.tox))
-    , events(std::move(rhs.events))
-    , mutex(std::move(rhs.mutex))
-    { rhs.live = false; }
-
-    // Move members from another object into this existing one, then set the right hand side to the DEAD state.
-    // This object is then live again.
-    Tox4jStruct &operator=(Tox4jStruct &&rhs) {
-        assert(!live);
-
-        tox = std::move(rhs.tox);
-        events = std::move(rhs.events);
-        mutex = std::move(rhs.mutex);
-        rhs.live = false;
-        this->live = true;
-
-        return *this;
-    }
-};
-// This struct should remain small. Check some assumptions here.
-static_assert(sizeof(Tox4jStruct) == sizeof(void *) * 4,
-    "Tox4jStruct has unexpected members or padding");
-
-class ToxInstances {
-    std::vector<Tox4jStruct> instances;
-    std::vector<jint> freelist;
-
-public:
-    std::mutex mutex;
-
-    bool isValid(jint instance_number) const {
-        return instance_number > 0
-            && (size_t) instance_number <= instances.size();
-    }
-
-    bool isFree(jint instance_number) const {
-        return std::find(freelist.begin(), freelist.end(), instance_number) != freelist.end();
-    }
-
-    void setFree(jint instance_number) {
-        freelist.push_back(instance_number);
-    }
-
-    Tox4jStruct const &operator[](jint instance_number) const {
-        return instances[instance_number - 1];
-    }
-
-    jint add(Tox4jStruct &&instance) {
-        // If there are free objects we can reuse..
-        if (!freelist.empty()) {
-            // ..use the last object that became unreachable (it will most likely be in cache).
-            jint instance_number = freelist.back();
-            assert(instance_number >= 1);
-            freelist.pop_back(); // Remove it from the free list.
-            assert(instances[instance_number - 1].isDead());
-            instances[instance_number - 1] = std::move(instance);
-            assert(instances[instance_number - 1].isLive());
-            return instance_number;
-        }
-
-        // Otherwise, add a new one.
-        instances.push_back(std::move(instance));
-        return (jint) instances.size();
-    }
-
-    Tox4jStruct remove(jint instance_number) {
-        return std::move(instances[instance_number - 1]);
-    }
-
-    void destroyAll() {
-        for (Tox4jStruct &instance : instances) {
-            Tox4jStruct dying(std::move(instance));
-        }
-    }
-
-    bool empty() const { return instances.empty(); }
-    size_t size() const { return instances.size(); }
-};
-
-static ToxInstances instances;
-
-
-static std::string fullMessage(jint instance_number, char const *message) {
-    std::ostringstream result;
-    result << message << ", instance_number = " << instance_number;
-    return result.str();
-}
-
-static inline void throw_tox_killed_exception(JNIEnv *env, jint instance_number, char const *message) {
-    env->ThrowNew(env->FindClass("im/tox/tox4j/exceptions/ToxKilledException"),
-        fullMessage(instance_number, message).c_str());
-}
-
-static inline void throw_illegal_state_exception(JNIEnv *env, jint instance_number, char const *message) {
-    env->ThrowNew(env->FindClass("java/lang/IllegalStateException"),
-        fullMessage(instance_number, message).c_str());
-}
-static inline void throw_illegal_state_exception(JNIEnv *env, jint instance_number, std::string const &message) {
-    throw_illegal_state_exception(env, instance_number, message.c_str());
-}
-
-static inline void tox4j_assert(bool condition, JNIEnv *env, char const *message) {
-    if (!condition) {
-        env->FatalError(message);
-    }
-}
-
-template<typename T> T default_value() { return T(); }
-template<> void default_value<void>() { }
-
-template<typename Func>
-static typename std::result_of<Func(Tox *, ToxEvents &)>::type
-with_instance(JNIEnv *env, jint instance_number, Func func)
-{
-    typedef typename std::result_of<Func(Tox *, ToxEvents &)>::type return_type;
-
-    if (instance_number == 0) {
-        throw_illegal_state_exception(env, instance_number, "Function called on incomplete object");
-        return default_value<return_type>();
-    }
-
-    std::unique_lock<std::mutex> lock(instances.mutex);
-    if (!instances.isValid(instance_number)) {
-        throw_tox_killed_exception(env, instance_number, "Tox function invoked on invalid tox instance");
-        return default_value<return_type>();
-    }
-
-    Tox4jStruct const &instance = instances[instance_number];
-
-    if (!instance.isLive()) {
-        throw_tox_killed_exception(env, instance_number, "Tox function invoked on killed tox instance");
-        return default_value<return_type>();
-    }
-
-    std::lock_guard<std::mutex> ilock(*instance.mutex);
-    Tox *tox = instance.tox.get();
-    ToxEvents &events = *instance.events;
-    lock.unlock();
-    return func(tox, events);
-}
+#include "Tox4j.h"
 
 /*
  * Do setup here. Caching of needed java method IDs etc should be done in this function. It is guaranteed to be called
@@ -194,8 +9,8 @@ jint JNI_OnLoad(JavaVM *, void *) {
 }
 
 JNIEXPORT void JNICALL Java_im_tox_tox4j_Tox4j_destroyAll(JNIEnv *, jclass) {
-    std::unique_lock<std::mutex> lock(instances.mutex);
-    instances.destroyAll();
+    std::unique_lock<std::mutex> lock(ToxInstances::self.mutex);
+    ToxInstances::self.destroyAll();
 }
 
 JNIEXPORT jint JNICALL Java_im_tox_tox4j_Tox4j_toxNew(JNIEnv *env, jclass, jboolean ipv6enabled, jboolean udpDisabled,
@@ -223,25 +38,25 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_Tox4j_toxNew(JNIEnv *env, jclass, jbool
     };
 
     // This lock guards the instance manager.
-    std::lock_guard<std::mutex> lock(instances.mutex);
-    return instances.add(std::move(instance));
+    std::lock_guard<std::mutex> lock(ToxInstances::self.mutex);
+    return ToxInstances::self.add(std::move(instance));
 }
 
 JNIEXPORT void JNICALL Java_im_tox_tox4j_Tox4j_kill(JNIEnv *env, jclass, jint instance_number) {
-    std::lock_guard<std::mutex> lock(instances.mutex);
+    std::lock_guard<std::mutex> lock(ToxInstances::self.mutex);
 
     if (instance_number < 0) {
         throw_illegal_state_exception(env, instance_number, "Tox instance out of range");
         return;
     }
 
-    if (!instances.isValid(instance_number)) {
+    if (!ToxInstances::self.isValid(instance_number)) {
         throw_tox_killed_exception(env, instance_number, "close called on invalid instance");
         return;
     }
 
     // After this move, the pointers in instance_vector[instance_number] will all be nullptr...
-    Tox4jStruct dying(instances.remove(instance_number));
+    Tox4jStruct dying(ToxInstances::self.remove(instance_number));
 
     // ... so that this check will fail, if the function is called twice on the same instance.
     if (!dying.tox) {
@@ -259,25 +74,25 @@ JNIEXPORT void JNICALL Java_im_tox_tox4j_Tox4j_finalize(JNIEnv *env, jclass, jin
         return;
     }
 
-    if (instances.empty()) {
+    if (ToxInstances::self.empty()) {
         throw_illegal_state_exception(env, instance_number, "Tox instance manager is empty");
         return;
     }
 
-    std::lock_guard<std::mutex> lock(instances.mutex);
-    if (!instances.isValid(instance_number)) {
+    std::lock_guard<std::mutex> lock(ToxInstances::self.mutex);
+    if (!ToxInstances::self.isValid(instance_number)) {
         throw_illegal_state_exception(env, instance_number,
-            "Tox instance out of range (max: " + std::to_string(instances.size() - 1) + ")");
+            "Tox instance out of range (max: " + std::to_string(ToxInstances::self.size() - 1) + ")");
         return;
     }
 
     // An instance should never be on this list twice.
-    if (instances.isFree(instance_number)) {
+    if (ToxInstances::self.isFree(instance_number)) {
         throw_illegal_state_exception(env, instance_number, "Tox instance already on free list");
         return;
     }
 
-    instances.setFree(instance_number);
+    ToxInstances::self.setFree(instance_number);
 }
 
 JNIEXPORT jint JNICALL Java_im_tox_tox4j_Tox4j_bootstrap(JNIEnv *env, jclass, jint instance_number,
---
+++ src/main/cpp/Tox4j.h
@@ -0,0 +1,187 @@
+#include <algorithm>
+#include <vector>
+#include <mutex>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+
+#include "JTox.h"
+#include "jniutil.h"
+#include "events.pb.h"
+
+#include <tox/tox.h>
+
+using tox4j::proto::ToxEvents;
+
+struct ToxDeleter {
+    void operator()(Tox *tox) {
+        tox_kill(tox);
+    }
+};
+
+class Tox4jStruct {
+    // Objects of this class can conceptionally have three states:
+    // - LIVE: A tox instance is alive and running.
+    // - DEAD: The object is empty, all pointers are nullptr.
+    // - COLLECTED: state is DEAD, and the object's instance_number is in instance_freelist.
+    bool live = true;
+
+public:
+    std::unique_ptr<Tox, ToxDeleter> tox;
+    std::unique_ptr<ToxEvents> events;
+    std::unique_ptr<std::mutex> mutex;
+
+    bool isLive() const { return live; }
+    bool isDead() const { return !live; }
+
+    Tox4jStruct(std::unique_ptr<Tox, ToxDeleter> &&tox,
+                std::unique_ptr<ToxEvents> &&events,
+                std::unique_ptr<std::mutex> &&mutex)
+    : tox(std::move(tox))
+    , events(std::move(events))
+    , mutex(std::move(mutex))
+    { }
+
+    // Move members from another object into this new one, then set the old one to the DEAD state.
+    Tox4jStruct(Tox4jStruct &&rhs)
+    : tox(std::move(rhs.tox))
+    , events(std::move(rhs.events))
+    , mutex(std::move(rhs.mutex))
+    { rhs.live = false; }
+
+    // Move members from another object into this existing one, then set the right hand side to the DEAD state.
+    // This object is then live again.
+    Tox4jStruct &operator=(Tox4jStruct &&rhs) {
+        assert(!live);
+
+        tox = std::move(rhs.tox);
+        events = std::move(rhs.events);
+        mutex = std::move(rhs.mutex);
+        rhs.live = false;
+        this->live = true;
+
+        return *this;
+    }
+};
+// This struct should remain small. Check some assumptions here.
+static_assert(sizeof(Tox4jStruct) == sizeof(void *) * 4,
+    "Tox4jStruct has unexpected members or padding");
+
+class ToxInstances {
+    std::vector<Tox4jStruct> instances;
+    std::vector<jint> freelist;
+
+public:
+    std::mutex mutex;
+
+    bool isValid(jint instance_number) const {
+        return instance_number > 0
+            && (size_t) instance_number <= instances.size();
+    }
+
+    bool isFree(jint instance_number) const {
+        return std::find(freelist.begin(), freelist.end(), instance_number) != freelist.end();
+    }
+
+    void setFree(jint instance_number) {
+        freelist.push_back(instance_number);
+    }
+
+    Tox4jStruct const &operator[](jint instance_number) const {
+        return instances[instance_number - 1];
+    }
+
+    jint add(Tox4jStruct &&instance) {
+        // If there are free objects we can reuse..
+        if (!freelist.empty()) {
+            // ..use the last object that became unreachable (it will most likely be in cache).
+            jint instance_number = freelist.back();
+            assert(instance_number >= 1);
+            freelist.pop_back(); // Remove it from the free list.
+            assert(instances[instance_number - 1].isDead());
+            instances[instance_number - 1] = std::move(instance);
+            assert(instances[instance_number - 1].isLive());
+            return instance_number;
+        }
+
+        // Otherwise, add a new one.
+        instances.push_back(std::move(instance));
+        return (jint) instances.size();
+    }
+
+    Tox4jStruct remove(jint instance_number) {
+        return std::move(instances[instance_number - 1]);
+    }
+
+    void destroyAll() {
+        for (Tox4jStruct &instance : instances) {
+            Tox4jStruct dying(std::move(instance));
+        }
+    }
+
+    bool empty() const { return instances.empty(); }
+    size_t size() const { return instances.size(); }
+
+    static ToxInstances self;
+};
+
+
+static inline std::string fullMessage(jint instance_number, char const *message) {
+    std::ostringstream result;
+    result << message << ", instance_number = " << instance_number;
+    return result.str();
+}
+
+static inline void throw_tox_killed_exception(JNIEnv *env, jint instance_number, char const *message) {
+    env->ThrowNew(env->FindClass("im/tox/tox4j/exceptions/ToxKilledException"),
+        fullMessage(instance_number, message).c_str());
+}
+
+static inline void throw_illegal_state_exception(JNIEnv *env, jint instance_number, char const *message) {
+    env->ThrowNew(env->FindClass("java/lang/IllegalStateException"),
+        fullMessage(instance_number, message).c_str());
+}
+static inline void throw_illegal_state_exception(JNIEnv *env, jint instance_number, std::string const &message) {
+    throw_illegal_state_exception(env, instance_number, message.c_str());
+}
+
+static inline void tox4j_assert(bool condition, JNIEnv *env, char const *message) {
+    if (!condition) {
+        env->FatalError(message);
+    }
+}
+
+template<typename T> static inline T default_value() { return T(); }
+template<> inline void default_value<void>() { }
+
+template<typename Func>
+static typename std::result_of<Func(Tox *, ToxEvents &)>::type
+with_instance(JNIEnv *env, jint instance_number, Func func)
+{
+    typedef typename std::result_of<Func(Tox *, ToxEvents &)>::type return_type;
+
+    if (instance_number == 0) {
+        throw_illegal_state_exception(env, instance_number, "Function called on incomplete object");
+        return default_value<return_type>();
+    }
+
+    std::unique_lock<std::mutex> lock(ToxInstances::self.mutex);
+    if (!ToxInstances::self.isValid(instance_number)) {
+        throw_tox_killed_exception(env, instance_number, "Tox function invoked on invalid tox instance");
+        return default_value<return_type>();
+    }
+
+    Tox4jStruct const &instance = ToxInstances::self[instance_number];
+
+    if (!instance.isLive()) {
+        throw_tox_killed_exception(env, instance_number, "Tox function invoked on killed tox instance");
+        return default_value<return_type>();
+    }
+
+    std::lock_guard<std::mutex> ilock(*instance.mutex);
+    Tox *tox = instance.tox.get();
+    ToxEvents &events = *instance.events;
+    lock.unlock();
+    return func(tox, events);
+}
