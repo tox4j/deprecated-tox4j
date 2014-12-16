f70f88134891acc61014440b62d263682cba938f
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-12-16 11:28:58 +0000

    Implement the C++ side callbacks with protobufs.

--- src/main/cpp/core.cpp
+++ src/main/cpp/core.cpp
@@ -35,6 +35,181 @@ throw_tox_exception(JNIEnv *env, char const *method, char const *code)
 }
 
 
+static void tox4j_connection_status_cb(Tox *tox, bool is_connected, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_connectionstatus();
+    msg->set_isconnected(is_connected);
+}
+static void tox4j_friend_name_cb(Tox *tox, uint32_t friend_number, uint8_t const *name, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendname();
+    msg->set_friendnumber(friend_number);
+    msg->set_name(name, length);
+}
+static void tox4j_friend_status_message_cb(Tox *tox, uint32_t friend_number, uint8_t const *message, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendstatusmessage();
+    msg->set_friendnumber(friend_number);
+    msg->set_message(message, length);
+}
+static void tox4j_friend_status_cb(Tox *tox, uint32_t friend_number, TOX_STATUS status, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendstatus();
+    msg->set_friendnumber(friend_number);
+
+    using im::tox::tox4j::proto::FriendStatus;
+    switch (status) {
+        case TOX_STATUS_NONE:
+            msg->set_status(FriendStatus::NONE);
+            break;
+        case TOX_STATUS_AWAY:
+            msg->set_status(FriendStatus::AWAY);
+            break;
+        case TOX_STATUS_BUSY:
+            msg->set_status(FriendStatus::BUSY);
+            break;
+    }
+}
+static void tox4j_friend_connected_cb(Tox *tox, uint32_t friend_number, bool is_connected, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendconnected();
+    msg->set_friendnumber(friend_number);
+    msg->set_isconnected(is_connected);
+}
+static void tox4j_friend_typing_cb(Tox *tox, uint32_t friend_number, bool is_typing, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendtyping();
+    msg->set_friendnumber(friend_number);
+    msg->set_istyping(is_typing);
+}
+static void tox4j_read_receipt_cb(Tox *tox, uint32_t friend_number, uint32_t message_id, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_readreceipt();
+    msg->set_friendnumber(friend_number);
+    msg->set_messageid(message_id);
+}
+static void tox4j_friend_request_cb(Tox *tox, uint8_t const *client_id, /*uint32_t time_delta, */uint8_t const *message, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendrequest();
+    msg->set_clientid(client_id, TOX_CLIENT_ID_SIZE);
+    msg->set_timedelta(0);
+    msg->set_message(message, length);
+}
+static void tox4j_friend_message_cb(Tox *tox, uint32_t friend_number, /*uint32_t time_delta, */uint8_t const *message, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendmessage();
+    msg->set_friendnumber(friend_number);
+    msg->set_timedelta(0);
+    msg->set_message(message, length);
+}
+static void tox4j_friend_action_cb(Tox *tox, uint32_t friend_number, /*uint32_t time_delta, */uint8_t const *action, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_friendaction();
+    msg->set_friendnumber(friend_number);
+    msg->set_timedelta(0);
+    msg->set_action(action, length);
+}
+static void tox4j_file_control_cb(Tox *tox, uint32_t friend_number, uint8_t file_number, TOX_FILE_CONTROL control, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_filecontrol();
+    msg->set_friendnumber(friend_number);
+    msg->set_filenumber(file_number);
+
+    using im::tox::tox4j::proto::FileControl;
+    switch (control) {
+        case TOX_FILE_CONTROL_RESUME:
+            msg->set_control(FileControl::RESUME);
+            break;
+        case TOX_FILE_CONTROL_PAUSE:
+            msg->set_control(FileControl::PAUSE);
+            break;
+        case TOX_FILE_CONTROL_CANCEL:
+            msg->set_control(FileControl::CANCEL);
+            break;
+    }
+}
+static size_t tox4j_file_send_chunk_cb(Tox *tox, uint32_t friend_number, uint8_t file_number, uint64_t position, uint8_t *data, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_filesendchunk();
+    msg->set_friendnumber(friend_number);
+    msg->set_filenumber(file_number);
+    msg->set_position(position);
+    msg->set_data(data, length);
+    return 0; // TODO: this is BROKEN!
+}
+static void tox4j_file_recv_cb(Tox *tox, uint32_t friend_number, uint8_t file_number, TOX_FILE_KIND kind, uint64_t file_size, uint8_t const *filename, size_t filename_length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_filereceive();
+    msg->set_friendnumber(friend_number);
+    msg->set_filenumber(file_number);
+
+    using im::tox::tox4j::proto::FileReceive;
+    switch (kind) {
+        case TOX_FILE_KIND_DATA:
+            msg->set_kind(FileReceive::DATA);
+            break;
+        case TOX_FILE_KIND_AVATAR:
+            msg->set_kind(FileReceive::AVATAR);
+            break;
+    }
+
+    msg->set_filesize(file_size);
+    msg->set_filename(filename, filename_length);
+}
+static void tox4j_file_recv_chunk_cb(Tox *tox, uint32_t friend_number, uint8_t file_number, uint64_t position, uint8_t const *data, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_filereceivechunk();
+    msg->set_friendnumber(friend_number);
+    msg->set_filenumber(file_number);
+    msg->set_position(position);
+    msg->set_data(data, length);
+}
+static void tox4j_lossy_packet_cb(Tox *tox, uint32_t friend_number, uint8_t const *data, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_lossypacket();
+    msg->set_friendnumber(friend_number);
+    msg->set_data(data, length);
+}
+static void tox4j_lossless_packet_cb(Tox *tox, uint32_t friend_number, uint8_t const *data, size_t length, void *user_data)
+{
+    unused(tox);
+    ToxEvents &events = *static_cast<ToxEvents *>(user_data);
+    auto msg = events.add_losslesspacket();
+    msg->set_friendnumber(friend_number);
+    msg->set_data(data, length);
+}
+
+
 /*
  * Class:     im_tox_tox4j_v2_ToxCoreImpl
  * Method:    destroyAll
@@ -58,7 +233,7 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxNew
     auto opts = Tox_Options();
     opts.ipv6_enabled = ipv6Enabled;
     opts.udp_enabled = udpEnabled;
-    opts.proxy_type = (TOX_PROXY_TYPE) proxyType; // TODO: validate
+    opts.proxy_type = (TOX_PROXY_TYPE) proxyType;
 
     std::vector<char> proxy_address;
     if (proxyAddress == nullptr) {
@@ -73,10 +248,32 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxNew
     switch (error) {
         case TOX_ERR_NEW_OK: {
             assert(tox != nullptr);
+
+            // Create the master events object.
+            std::unique_ptr<ToxEvents> events(new ToxEvents);
+
+            // Set up our callbacks.
+            tox_callback_connection_status    (tox.get(), tox4j_connection_status_cb,     events.get());
+            tox_callback_friend_name          (tox.get(), tox4j_friend_name_cb,           events.get());
+            tox_callback_friend_status_message(tox.get(), tox4j_friend_status_message_cb, events.get());
+            tox_callback_friend_status        (tox.get(), tox4j_friend_status_cb,         events.get());
+            tox_callback_friend_connected     (tox.get(), tox4j_friend_connected_cb,      events.get());
+            tox_callback_friend_typing        (tox.get(), tox4j_friend_typing_cb,         events.get());
+            tox_callback_read_receipt         (tox.get(), tox4j_read_receipt_cb,          events.get());
+            tox_callback_friend_request       (tox.get(), tox4j_friend_request_cb,        events.get());
+            tox_callback_friend_message       (tox.get(), tox4j_friend_message_cb,        events.get());
+            tox_callback_friend_action        (tox.get(), tox4j_friend_action_cb,         events.get());
+            tox_callback_file_control         (tox.get(), tox4j_file_control_cb,          events.get());
+            tox_callback_file_send_chunk      (tox.get(), tox4j_file_send_chunk_cb,       events.get());
+            tox_callback_file_recv            (tox.get(), tox4j_file_recv_cb,             events.get());
+            tox_callback_file_recv_chunk      (tox.get(), tox4j_file_recv_chunk_cb,       events.get());
+            tox_callback_lossy_packet         (tox.get(), tox4j_lossy_packet_cb,          events.get());
+            tox_callback_lossless_packet      (tox.get(), tox4j_lossless_packet_cb,       events.get());
+
             // We can create the new instance outside ToxInstances' critical section.
             Tox4jStruct instance {
                 std::move(tox),
-                std::unique_ptr<ToxEvents>(new ToxEvents),
+                std::move(events),
                 std::unique_ptr<std::mutex>(new std::mutex)
             };
 
@@ -186,6 +383,7 @@ JNIEXPORT jbyteArray JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxSave
   (JNIEnv *env, jclass, jint instanceNumber)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         std::vector<uint8_t> buffer(tox_save_size(tox));
         tox_save(tox, buffer.data());
 
@@ -202,6 +400,7 @@ JNIEXPORT void JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxLoad
   (JNIEnv *env, jclass, jint instanceNumber, jbyteArray data)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         ByteArray bytes(env, data);
         TOX_ERR_LOAD error;
         tox_load(tox, bytes.data(), bytes.length(), &error);
@@ -232,6 +431,7 @@ JNIEXPORT void JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxBootstrap
   (JNIEnv *env, jclass, jint instanceNumber, jstring address, jint port, jbyteArray public_key)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         TOX_ERR_BOOTSTRAP error;
         tox_bootstrap(tox, UTFChars(env, address), port, ByteArray(env, public_key), &error);
         switch (error) {
@@ -278,6 +478,7 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxIterationTime
   (JNIEnv *env, jclass, jint instanceNumber)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         return tox_iteration_time(tox);
     });
 }
@@ -377,6 +578,7 @@ JNIEXPORT jbyteArray JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxGetAddress
   (JNIEnv *env, jclass, jint instanceNumber)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         std::vector<uint8_t> address(TOX_ADDRESS_SIZE);
         tox_get_address(tox, address.data());
 
@@ -485,7 +687,7 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxGetStatus
 
 
 static uint32_t
-handle_tox_add_friend_result(JNIEnv *env, jint instanceNumber, uint32_t friend_number, TOX_ERR_ADD_FRIEND error)
+handle_tox_add_friend_result(JNIEnv *env, uint32_t friend_number, TOX_ERR_ADD_FRIEND error)
 {
     switch (error) {
         case TOX_ERR_ADD_FRIEND_OK:
@@ -530,10 +732,11 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxAddFriend
   (JNIEnv *env, jclass, jint instanceNumber, jbyteArray address, jbyteArray message)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         TOX_ERR_ADD_FRIEND error;
         ByteArray messageBytes(env, message);
         uint32_t friend_number = tox_add_friend(tox, ByteArray(env, address), messageBytes.data(), messageBytes.length(), &error);
-        return handle_tox_add_friend_result(env, instanceNumber, friend_number, error);
+        return handle_tox_add_friend_result(env, friend_number, error);
     });
 }
 
@@ -546,9 +749,10 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxAddFriendNorequest
   (JNIEnv *env, jclass, jint instanceNumber, jbyteArray clientId)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         TOX_ERR_ADD_FRIEND error;
         uint32_t friend_number = tox_add_friend_norequest(tox, ByteArray(env, clientId), &error);
-        return handle_tox_add_friend_result(env, instanceNumber, friend_number, error);
+        return handle_tox_add_friend_result(env, friend_number, error);
     });
 }
 
@@ -577,6 +781,7 @@ JNIEXPORT jint JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxGetFriendNumber
   (JNIEnv *env, jclass, jint instanceNumber, jbyteArray clientId)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         TOX_ERR_GET_FRIEND_NUMBER error;
         uint32_t friend_number = tox_get_friend_number(tox, ByteArray(env, clientId), &error);
         switch (error) {
@@ -604,6 +809,7 @@ JNIEXPORT jbyteArray JNICALL Java_im_tox_tox4j_v2_ToxCoreImpl_toxGetFriendClient
   (JNIEnv *env, jclass, jint instanceNumber, jint friendNumber)
 {
     return with_instance(env, instanceNumber, [=](Tox *tox, ToxEvents &events) {
+        unused(events);
         TOX_ERR_GET_CLIENT_ID error;
         std::vector<uint8_t> buffer(TOX_CLIENT_ID_SIZE);
         jbyteArray result = nullptr;
