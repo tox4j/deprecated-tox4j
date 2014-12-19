bfb7924060da46c49ae212809fa4d7f670409474
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-12-19 04:39:22 +0000

    Add very simple beginning of client library + spec.
    And a dummy Main that does nothing for now.

---
+++ src/main/java/im/tox/client/ToxClient.scala
@@ -0,0 +1,19 @@
+package im.tox.client
+
+import im.tox.tox4j.enums.ToxStatus
+import im.tox.tox4j.{ToxOptions, ToxCoreImpl}
+
+class ToxClient {
+
+  private val tox = new ToxCoreImpl(new ToxOptions)
+
+  def getName: String = new String(tox.getName)
+  def setName(name: String) = tox.setName(name.getBytes)
+
+  def getStatus: ToxStatus = tox.getStatus
+  def setStatus(status: ToxStatus) = tox.setStatus(status)
+
+  def getStatusMessage: String = new String(tox.getStatusMessage)
+  def setStatusMessage(message: String) = tox.setStatusMessage(message.getBytes)
+
+}
