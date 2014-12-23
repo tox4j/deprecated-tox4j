797c7f65aad9c94d1d9d3299261bbd1b0feaa10b
Pippijn van Steenhoven <pip88nl@gmail.com>
2014-12-23 13:39:36 +0000

    Move networking tests to a separate class.
    This allows us to run simple tests more frequently (all the time).

--- src/test/java/im/tox/tox4j/ToxCoreTest.java
+++ src/test/java/im/tox/tox4j/ToxCoreTest.java
@@ -12,41 +12,6 @@ import static org.junit.Assert.*;
 
 public class ToxCoreTest extends ToxCoreImplTestBase {
 
-    private static final boolean SLOW_TESTS = true;
-    private static final int TOX_COUNT = 10;
-
-    private void testBootstrap(String ipv4, int port, byte[] dhtId) throws Exception {
-        if (!SLOW_TESTS) return;
-        try (ToxCore tox = newTox()) {
-            long start = System.currentTimeMillis();
-            tox.bootstrap(ipv4, port, dhtId);
-            ConnectedListener status = new ConnectedListener();
-            tox.callbackConnectionStatus(status);
-            while (!status.isConnected()) {
-                tox.iteration();
-                try {
-                    Thread.sleep(tox.iterationInterval());
-                } catch (InterruptedException e) {
-                    // Probably the timeout was reached, so we ought to be killed soon.
-                }
-            }
-            long end = System.currentTimeMillis();
-            if (LOGGING) System.out.println("Bootstrap to remote bootstrap node took " + (end - start) + "ms");
-        }
-    }
-
-    @Test(timeout = TIMEOUT)
-    public void testBootstrap4() throws Exception {
-        assumeIPv4();
-        testBootstrap(nodes[0].ipv4, nodes[0].port, nodes[0].dhtId);
-    }
-
-    @Test(timeout = TIMEOUT)
-    public void testBootstrap6() throws Exception {
-        assumeIPv6();
-        testBootstrap(nodes[0].ipv6, nodes[0].port, nodes[0].dhtId);
-    }
-
     @Test
     public void testToxNew() throws Exception {
         ToxOptions options = new ToxOptions();
@@ -130,50 +95,6 @@ public class ToxCoreTest extends ToxCoreImplTestBase {
     public void testBootstrapBorderlinePort2() throws Exception {
         try (ToxCore tox = newTox()) {
             tox.bootstrap(nodes[0].ipv4, 65535, new byte[ToxConstants.CLIENT_ID_SIZE]);
-        }
-    }
-
-    @Test(timeout = TIMEOUT)
-    public void testBootstrapSelf() throws Exception {
-        // TODO: don't know how to test this on localhost
-    }
-
-    @Test(timeout = TIMEOUT)
-    public void testLANDiscoveryAll() throws Exception {
-        if (!SLOW_TESTS) return;
-        try (ToxList toxes = new ToxList(TOX_COUNT)) {
-            long start = System.currentTimeMillis();
-            // TODO: Generous timeout required for this; should be made more reliable.
-            while (!toxes.isAllConnected()) {
-                toxes.iteration();
-                try {
-                    Thread.sleep(toxes.iterationInterval());
-                } catch (InterruptedException e) {
-                    // Probably the timeout was reached, so we ought to be killed soon.
-                }
-            }
-            long end = System.currentTimeMillis();
-            if (LOGGING) System.out.println("Connecting all of " + toxes.size() + " toxes with LAN discovery " +
-                    "took " + (end - start) + "ms");
-        }
-    }
-
-    @Test(timeout = TIMEOUT)
-    public void testLANDiscoveryAny() throws Exception {
-        if (!SLOW_TESTS) return;
-        try (ToxList toxes = new ToxList(TOX_COUNT)) {
-            long start = System.currentTimeMillis();
-            while (!toxes.isAnyConnected()) {
-                toxes.iteration();
-                try {
-                    Thread.sleep(toxes.iterationInterval());
-                } catch (InterruptedException e) {
-                    // Probably the timeout was reached, so we ought to be killed soon.
-                }
-            }
-            long end = System.currentTimeMillis();
-            if (LOGGING) System.out.println("Connecting one of " + toxes.size() + " toxes with LAN discovery " +
-                    "took " + (end - start) + "ms");
         }
     }
 
