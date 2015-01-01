3582160a321e5d0eab5d9cfe629b40957c1ee744
Anonymized <anonymous@example.com>
2015-01-01 01:03:15 +0000

    Give RepeatedLanDiscoveryTest a correct main() signature.

--- src/test/java/im/tox/tox4j/core/RepeatedLanDiscoveryTest.java
+++ src/test/java/im/tox/tox4j/core/RepeatedLanDiscoveryTest.java
@@ -5,7 +5,7 @@ import im.tox.tox4j.ToxCoreImpl;
 
 public final class RepeatedLanDiscoveryTest {
 
-    public static void main() throws Exception {
+    public static void main(String[] args) throws Exception {
         for (int i = 0; i < 1000; i++) {
             System.out.println("Cycle " + i);
             try (ToxCore tox1 = new ToxCoreImpl()) {
@@ -14,10 +14,17 @@ public final class RepeatedLanDiscoveryTest {
                     tox1.callbackConnectionStatus(status);
                     tox2.callbackConnectionStatus(status);
 
+                    long start = System.currentTimeMillis();
+                    long last = start;
                     while (!status.isConnected()) {
                         tox1.iteration();
                         tox2.iteration();
                         Thread.sleep(Math.max(tox1.iterationInterval(), tox2.iterationInterval()));
+                        long now = System.currentTimeMillis();
+                        if ((now - last) > 10000) {
+                            System.out.println("Cycle is taking a long time: " + (now - start) + "ms");
+                            last = now;
+                        }
                     }
                 }
             }
