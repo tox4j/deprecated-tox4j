f0bac1f869c07cb1f5c7700d12aea91ae46d6956
Anonymized <anonymous@example.com>
2015-01-13 11:57:45 +0000

    Change TestClient to support multiple toxes.

--- src/test/scala/TestClient.scala
+++ src/test/scala/TestClient.scala
@@ -32,87 +32,113 @@ object TestClient extends App {
     }
   }
 
+  (args match {
+    case Array("--bootstrap", host, port, key, count) =>
+      (Some(host, Integer.parseInt(port), key), Integer.parseInt(count))
+    case Array("--bootstrap", host, port, key) =>
+      (Some(host, Integer.parseInt(port), key), 1)
+    case Array("--bootstrap", count) =>
+      (Some("144.76.60.215", 33445, "04119E835DF3E78BACF0F84235B300546AF8B936F035185E2A8E9E0A67C8924F"), Integer.parseInt(count))
+    case Array(count) =>
+      (None, Integer.parseInt(count))
+    case _ =>
+      (None, 1)
+  }) match {
+    case (bootstrap, count) =>
+      logger.info(s"Creating $count toxes")
 
-  val tox = new ToxCoreImpl({
-    val options = new ToxOptions
-    options.setIpv6Enabled(true)
-    options.setUdpEnabled(false)
-    options
-  })
+      val toxes = (1 to count) map { id =>
+        val tox = new ToxCoreImpl({
+          val options = new ToxOptions
+          options.setIpv6Enabled(true)
+          options.setUdpEnabled(bootstrap.isEmpty)
+          options
+        })
 
-  tox.callback(new ToxEventListener {
+        tox.callback(new TestEventListener(id))
+
+        bootstrap match {
+          case Some((host, port, key)) =>
+            logger.info(s"[$id] Bootstrapping to $host:$port")
+            tox.bootstrap(host, port, parseClientId(key))
+          case None =>
+        }
+        tox
+      }
+
+      logger.info("Starting event loop")
+      while (true) {
+        toxes.foreach(_.iteration)
+        Thread.sleep(toxes.map(_.iterationInterval).max)
+      }
+  }
+
+  private sealed class TestEventListener(id: Int) extends ToxEventListener {
 
     override def friendStatus(friendNumber: Int, status: ToxStatus): Unit = {
-      logger.info(s"friendStatus($friendNumber, $status)")
+      logger.info(s"[$id] friendStatus($friendNumber, $status)")
     }
 
     override def friendTyping(friendNumber: Int, isTyping: Boolean): Unit = {
-      logger.info(s"friendTyping($friendNumber, $isTyping)")
+      logger.info(s"[$id] friendTyping($friendNumber, $isTyping)")
     }
 
     override def connectionStatus(connectionStatus: ToxConnection): Unit = {
-      logger.info(s"connectionStatus($connectionStatus)")
+      logger.info(s"[$id] connectionStatus($connectionStatus)")
     }
 
     override def friendName(friendNumber: Int, name: Array[Byte]): Unit = {
-      logger.info(s"friendName($friendNumber, ${new String(name)})")
+      logger.info(s"[$id] friendName($friendNumber, ${new String(name)})")
     }
 
     override def friendAction(friendNumber: Int, timeDelta: Int, message: Array[Byte]): Unit = {
-      logger.info(s"friendAction($friendNumber, $timeDelta, ${new String(message)})")
+      logger.info(s"[$id] friendAction($friendNumber, $timeDelta, ${new String(message)})")
     }
 
     override def friendMessage(friendNumber: Int, timeDelta: Int, message: Array[Byte]): Unit = {
-      logger.info(s"friendMessage($friendNumber, $timeDelta, ${new String(message)})")
+      logger.info(s"[$id] friendMessage($friendNumber, $timeDelta, ${new String(message)})")
     }
 
     override def friendLossyPacket(friendNumber: Int, data: Array[Byte]): Unit = {
-      logger.info(s"friendLossyPacket($friendNumber, ${new String(data)})")
+      logger.info(s"[$id] friendLossyPacket($friendNumber, ${new String(data)})")
     }
 
     override def fileReceive(friendNumber: Int, fileNumber: Int, kind: ToxFileKind, fileSize: Long, filename: Array[Byte]): Unit = {
-      logger.info(s"fileReceive($friendNumber, $fileNumber, $kind, $fileSize, ${new String(filename)}})")
+      logger.info(s"[$id] fileReceive($friendNumber, $fileNumber, $kind, $fileSize, ${new String(filename)}})")
     }
 
     override def friendRequest(clientId: Array[Byte], timeDelta: Int, message: Array[Byte]): Unit = {
-      logger.info(s"friendRequest($clientId, $timeDelta, ${new String(message)})")
+      logger.info(s"[$id] friendRequest($clientId, $timeDelta, ${new String(message)})")
     }
 
     override def fileRequestChunk(friendNumber: Int, fileNumber: Int, position: Long, length: Int): Unit = {
-      logger.info(s"fileRequestChunk($friendNumber, $fileNumber, $position, $length)")
+      logger.info(s"[$id] fileRequestChunk($friendNumber, $fileNumber, $position, $length)")
     }
 
     override def fileReceiveChunk(friendNumber: Int, fileNumber: Int, position: Long, data: Array[Byte]): Unit = {
-      logger.info(s"fileReceiveChunk($friendNumber, $fileNumber, $position, ${new String(data)})")
+      logger.info(s"[$id] fileReceiveChunk($friendNumber, $fileNumber, $position, ${new String(data)})")
     }
 
     override def friendLosslessPacket(friendNumber: Int, data: Array[Byte]): Unit = {
-      logger.info(s"friendLosslessPacket($friendNumber, ${new String(data)})")
+      logger.info(s"[$id] friendLosslessPacket($friendNumber, ${new String(data)})")
     }
 
     override def friendConnectionStatus(friendNumber: Int, connectionStatus: ToxConnection): Unit = {
-      logger.info(s"friendConnectionStatus($friendNumber, $connectionStatus)")
+      logger.info(s"[$id] friendConnectionStatus($friendNumber, $connectionStatus)")
     }
 
     override def fileControl(friendNumber: Int, fileNumber: Int, control: ToxFileControl): Unit = {
-      logger.info(s"fileControl($friendNumber, $fileNumber, $control)")
+      logger.info(s"[$id] fileControl($friendNumber, $fileNumber, $control)")
     }
 
     override def friendStatusMessage(friendNumber: Int, message: Array[Byte]): Unit = {
-      logger.info(s"friendStatusMessage($friendNumber, ${new String(message)})")
+      logger.info(s"[$id] friendStatusMessage($friendNumber, ${new String(message)})")
     }
 
     override def readReceipt(friendNumber: Int, messageId: Int): Unit = {
-      logger.info(s"readReceipt($friendNumber, $messageId)")
+      logger.info(s"[$id] readReceipt($friendNumber, $messageId)")
     }
 
-  })
-
-  tox.bootstrap("144.76.60.215", 33445, parseClientId("04119E835DF3E78BACF0F84235B300546AF8B936F035185E2A8E9E0A67C8924F"))
-
-  while (true) {
-    tox.iteration()
-    Thread.sleep(tox.iterationInterval())
   }
 
 }
