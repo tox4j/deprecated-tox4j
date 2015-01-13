18632331dd5fb1a74b01a6fc4ce8ca30c5e630d4
Anonymized <anonymous@example.com>
2015-01-13 16:26:00 +0000

    Select a node I experimentally found to be working.

--- src/test/java/im/tox/tox4j/DhtNodeSelector.java
+++ src/test/java/im/tox/tox4j/DhtNodeSelector.java
@@ -16,7 +16,7 @@ import static org.junit.Assume.assumeNotNull;
 public final class DhtNodeSelector {
 
     private static final Logger logger = LoggerFactory.getLogger(DhtNodeSelector.class);
-    private static DhtNode selectedNode = ToxCoreTestBase.nodeCandidates[0];
+    private static DhtNode selectedNode = ToxCoreTestBase.nodeCandidates[2];
 
     @NotNull
     public synchronized DhtNode node(ToxFactory factory) {
