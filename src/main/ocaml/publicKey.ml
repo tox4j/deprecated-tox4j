3374a0110666d0241db359fd1a8e3fc969a602b5
iphydf <iphydf@users.noreply.github.com>
2015-01-31 05:00:37 +0000

    Tox uses a very annoying distance function.
    For no reason at all, just because someone misunderstood Kademlia and
    refuses to fix it after being taught how it works, the Tox key distance
    function
    - loses 32 bits of precision (1 bit per byte)
    - behaves differently from Kademlia
    - is more complex
    - performs much worse

--- src/main/ocaml/publicKey.ml
+++ src/main/ocaml/publicKey.ml
@@ -49,15 +49,35 @@ let unpack ~buf =
   of_string (Message.Consume.string ~len:Box.public_key_size buf)
 
 
+let kad_distance dist a b =
+  for i = 0 to Box.public_key_size - 1 do
+    Char.to_int a.[i] lxor Char.to_int b.[i]
+    |> Char.of_int_exn
+    |> Bytes.set dist i
+  done
+
+
+let tox_distance dist a b =
+  let int8_t i =
+    if i >= 128 then
+      -(256 - i)
+    else
+      i
+  in
+
+  for i = 0 to Box.public_key_size - 1 do
+    abs (int8_t (Char.to_int a.[i]) lxor int8_t (Char.to_int b.[i]))
+    |> Char.of_int_exn
+    |> Bytes.set dist i
+  done
+
+
 let distance a b =
   let a = to_string a in
   let b = to_string b in
   let dist = Bytes.create Box.public_key_size in
 
-  for i = 0 to Box.public_key_size - 1 do
-    Char.to_int a.[i] lxor Char.to_int b.[i]
-    |> Char.of_int_exn
-    |> Bytes.set dist i
-  done;
+  (*kad_distance dist a b;*)
+  tox_distance dist a b;
 
   Bytes.unsafe_to_string dist
