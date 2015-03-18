f5b18583f1f18b7651d39d79bb1278fbaf8e603f
iphydf <iphydf@users.noreply.github.com>
2015-03-18 21:44:06 +0000

    Break off function-owned error codes to error decls.

---
+++ src/main/ocaml/apidsl/splitErrorCodes.ml
@@ -0,0 +1,20 @@
+open ApiAst
+open ApiFoldMap
+
+
+let fold_decl v state = function
+  | Decl_Function (type_name, lname, parameters, Err_List enumerators) ->
+      let state =
+        ReplaceDecl.prepend state [Decl_Error (lname, enumerators)]
+      in
+      state, Decl_Function (type_name, lname, parameters, Err_From lname)
+
+  | decl ->
+      ReplaceDecl.fold_decl v state decl
+
+
+let v = { default with fold_decl }
+
+
+let transform (symtab, decls) =
+  symtab, snd @@ ReplaceDecl.fold_decls v (ReplaceDecl.initial, ()) decls
