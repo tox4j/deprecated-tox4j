e412d11011d2ed95536f31fbbe2628031be6b8ed
iphydf <iphydf@users.noreply.github.com>
2015-03-18 02:12:15 +0000

    Teach ThisParam about static namespaces.

--- src/main/ocaml/apidsl/thisParams.ml
+++ src/main/ocaml/apidsl/thisParams.ml
@@ -2,21 +2,26 @@ open ApiAst
 open ApiMap
 
 
-let map_decl v state = function
+let map_decl v this_name = function
 
+  | Decl_Static (Decl_Namespace _)
   | Decl_Static (Decl_Function _) as decl ->
       decl
 
   | Decl_Function (type_name, lname, parameters, error_list) ->
-      let parameters = Param (TypeName.this, "tox") :: parameters in
+      let parameters = Param (TypeName.this, this_name) :: parameters in
       Decl_Function (type_name, lname, parameters, error_list)
 
+  | Decl_Class (lname, decls) ->
+      let decls = visit_list v.map_decl v lname decls in
+      Decl_Class (lname, decls)
+
   | decl ->
-      visit_decl v state decl
+      visit_decl v this_name decl
 
 
 let v = { default with map_decl }
 
 
 let transform decls =
-  visit_decls v () decls
+  visit_decls v "" decls
