fa28c17f1a135f7d979733e8589738f29b1cb5dd
iphydf <iphydf@users.noreply.github.com>
2015-04-20 14:15:34 +0100

    udpPort -> port.

--- src/test/java/im/tox/tox4j/DhtNode.scala
+++ src/test/java/im/tox/tox4j/DhtNode.scala
@@ -6,8 +6,8 @@ final case class DhtNode(ipv4: String, ipv6: String, udpPort: Int, tcpPort: Int,
     this(ipv4, ipv6, udpPort, tcpPort, ToxCoreTestBase.parsePublicKey(dhtId))
   }
 
-  def this(ipv4: String, ipv6: String, udpPort: Int, dhtId: String) {
-    this(ipv4, ipv6, udpPort, udpPort, dhtId)
+  def this(ipv4: String, ipv6: String, port: Int, dhtId: String) {
+    this(ipv4, ipv6, port, port, dhtId)
   }
 
 }
