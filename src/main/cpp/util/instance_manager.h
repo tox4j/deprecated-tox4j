8931074c3dd529081bc368ab338e2242663389b4
iphydf <iphydf@users.noreply.github.com>
2015-05-12 19:21:12 +0100

    Decrease instance_manager's critical section length.
    We now unlock the manager before calling the function in with_instance
    (but after locking the instance).

--- src/main/cpp/util/instance_manager.h
+++ src/main/cpp/util/instance_manager.h
@@ -36,20 +36,53 @@ private:
    * the client code will never need to see unique_ptrs, and can simply operate
    * on object pointers and events references.
    */
-  struct instance_pair
+  struct instance_pointers
   {
-    ObjectP object;
-    EventsP events;
+    ObjectP object_p;
+    EventsP events_p;
 
     explicit operator bool () const
+    { return static_cast<bool> (object_p); }
+
+
+    /**
+     * Contains the actual pointer/reference to the instance_pointers data.
+     * This is used so we can unlock the instance_manager after getting the
+     * instance and before calling the function in with_instance, which may take
+     * a long time and would unnecessarily block the manager during its execution.
+     */
+    struct locked
     {
-      // These come and go hand in hand.
-      assert (!object == !events);
-      return static_cast<bool> (object);
+      Object *object;
+      Events *events;
+      std::unique_lock<std::mutex> lock;
+
+      explicit operator bool () const
+      {
+        // These come and go hand in hand.
+        assert (!object == !events);
+        return object != nullptr;
+      }
+    };
+
+    /**
+     * Copies the pointers together with their lock into a single object without
+     * pointers or references into the instance_pointers object, which may be
+     * moved when its containing vector reallocates.
+     */
+    locked
+    get (std::unique_lock<std::mutex> lock) const
+    {
+      return {
+        object_p.get (),
+        events_p.get (),
+        std::move (lock),
+      };
     }
   };
 
-  std::vector<instance_pair> instances;
+  // The instances and locks lists are always equal in size (class invariant).
+  std::vector<instance_pointers> instances;
   // Locks are managed separately so we can use trivial move semantics to destroy
   // and reassign instance_pairs. A deque is used so we don't need an additional
   // indirection that vector<unique_ptr<mutex>> would have.
@@ -98,17 +131,25 @@ public:
   instance_manager &operator = (instance_manager const &) = delete;
 
 
+  /**
+   * Hands over management of the object and events pointers to the manager. The
+   * returned instance number can be used to call with_instance for access and
+   * kill/finalize for destruction/cleanup.
+   */
   jint
-  add (JNIEnv *env, ObjectP object, EventsP events)
+  add (JNIEnv *env, ObjectP object_p, EventsP events_p)
   {
+    // The manager is locked for the entire duration of this function. No access
+    // or mutation may happen during an add, because we might reallocate and
+    // move objects here.
     std::lock_guard<std::mutex> lock (mutex);
 
-    tox4j_assert (object);
-    tox4j_assert (events);
+    tox4j_assert (object_p);
+    tox4j_assert (events_p);
 
-    instance_pair instance = {
-      std::move (object),
-      std::move (events),
+    instance_pointers instance_p = {
+      std::move (object_p),
+      std::move (events_p),
     };
 
     // If there are free objects we can reuse..
@@ -123,13 +164,13 @@ public:
         // All instances on the freelist should be empty.
         tox4j_assert (!instances[instanceNumber - 1]);
 
-        instances[instanceNumber - 1] = std::move (instance);
+        instances[instanceNumber - 1] = std::move (instance_p);
 
         return instanceNumber;
       }
 
     // Otherwise, add a new one.
-    instances.push_back (std::move (instance));
+    instances.push_back (std::move (instance_p));
     locks.emplace_back ();
 
     tox4j_assert (instances.size () == locks.size ());
@@ -137,9 +178,17 @@ public:
   }
 
 
+  /**
+   * Destroys an instance, deallocating both the object and the events.
+   *
+   * This function is idempotent as long as there was no finalize call with the
+   * passed instance number in between.
+   */
   void
   kill (JNIEnv *env, jint instanceNumber)
   {
+    // Lock the manager, since we're going to access the lists while add() might
+    // be modifying them.
     std::lock_guard<std::mutex> lock (mutex);
 
     if (!check_instance_number (env, instanceNumber, false))
@@ -153,9 +202,20 @@ public:
   }
 
 
+  /**
+   * Add the instance number to the freelist, making it a candidate for reuse.
+   *
+   * The kill function *must* be called before calling this function (otherwise,
+   * undefined behaviour).
+   * This function is *not* idempotent. It will throw a Java exception if it's
+   * called twice on the same instance number (given there was no add in between
+   * that returned that instance number).
+   */
   void
   finalize (JNIEnv *env, jint instanceNumber)
   {
+    // Lock the manager, since we're going to access the lists while add() might
+    // be modifying them.
     std::lock_guard<std::mutex> lock (mutex);
 
     // Don't throw on null instances, but also don't put it on the freelist.
@@ -181,20 +241,36 @@ public:
   }
 
 
+  /**
+   * Access the instance identified by the instance number. Func must be a
+   * callable type and accept an Object* and an Events&. The result of the
+   * function call is returned by with_instance.
+   *
+   * For the duration of the call to Func, the instance will be locked, and the
+   * manager will be unlocked. This means that multiple concurrent calls to a
+   * single instance are not possible, but multiple concurrent calls to different
+   * instances is permitted.
+   */
   template<typename Func>
   auto
   with_instance (JNIEnv *env, jint instanceNumber, Func func)
   {
     typedef typename std::result_of<Func (Object *, Events &)>::type return_type;
 
-    std::lock_guard<std::mutex> lock (mutex);
+    // After this object is initialised, it is independent of the pointers'
+    // memory location in the manager.
+    auto instance = [this, env, instanceNumber]
+      {
+        // Lock the instance manager only while accessing the list.
+        std::lock_guard<std::mutex> lock (mutex);
 
-    if (!check_instance_number (env, instanceNumber, false))
-      return return_type ();
+        if (!check_instance_number (env, instanceNumber, false))
+          return typename instance_pointers::locked { };
 
-    std::lock_guard<std::mutex> instance_lock (locks[instanceNumber - 1]);
-
-    instance_pair &instance = instances.at (instanceNumber - 1);
+        // Lock the instance, get the pointers out of the instance vector, and
+        // unlock the manager (by lock_guard's destructor).
+        return instances[instanceNumber - 1].get (std::unique_lock<std::mutex> (locks[instanceNumber - 1]));
+      } ();
 
     if (!instance)
       {
@@ -202,6 +278,8 @@ public:
         return return_type ();
       }
 
-    return func (instance.object.get (), *instance.events);
+    // After this function returns, the unique_lock in the instance object will
+    // be destroyed, unlocking the instance.
+    return func (instance.object, *instance.events);
   }
 };
