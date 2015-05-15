3f2a266671b01311f9a60a6c22b9f6189908a63e
iphydf <iphydf@users.noreply.github.com>
2015-05-15 19:57:15 +0100

    Add missing <p> tag for javadoc.

--- src/test/java/im/tox/tox4j/core/FinalizerTest.java
+++ src/test/java/im/tox/tox4j/core/FinalizerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
  * These tests solely exist to exercise the C++ code paths that deal with closing and finalisation. If the C++ code has
  * errors in this area, these two tests can be used to single them out and debug them.
  *
+ * <p>
  * Although {@link System#gc()} doesn't necessarily perform a GC, on the Oracle JVM it actually does reliably do so.
  * Thus, these tests don't formally test anything, but in reality they do.
  */
