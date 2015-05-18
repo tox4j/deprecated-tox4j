aa826106e673252fce4d6df68f3dae8d99f582fa
iphydf <iphydf@users.noreply.github.com>
2015-05-18 22:46:49 +0100

    Use protobuf v2.4.1 on Android, because travis is ANCIENT.

--- android/build-deps.sh
+++ android/build-deps.sh
@@ -122,7 +122,8 @@ INSTALL() {
 (
   CLONE https://github.com/google protobuf
   #git checkout tags/v2.6.1
-  git checkout 0c995c930067241b40b10b0b01504c784cade03e
+  #git checkout tags/v2.5.0
+  git checkout tags/v2.4.1
   patch -p1 < ../protobuf.patch
   INSTALL protobuf --with-protoc=protoc
 )
