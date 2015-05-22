03abf12667799dafb2dbb4547ffe24d312cbd7c5
iphydf <iphydf@users.noreply.github.com>
2015-05-22 17:05:17 +0100

    Fix scalastyle warnings in test.

--- src/test/java/im/tox/tox4j/core/callbacks/FriendLosslessPacketCallbackTest.scala
+++ src/test/java/im/tox/tox4j/core/callbacks/FriendLosslessPacketCallbackTest.scala
@@ -2,17 +2,18 @@ package im.tox.tox4j.core.callbacks
 
 import im.tox.tox4j.AliceBobTestBase
 import im.tox.tox4j.AliceBobTestBase.ChatClient
+import im.tox.tox4j.AliceBobTestBase.ChatClient.Task
 import im.tox.tox4j.core.ToxCore
 import im.tox.tox4j.core.enums.ToxConnection
 import org.junit.Assert.assertEquals
 
 class FriendLosslessPacketCallbackTest extends AliceBobTestBase {
-  def newAlice = new ChatClient {
+  def newAlice: ChatClient = new ChatClient {
     override def friendConnectionStatus(friendNumber: Int, connection: ToxConnection): Unit = {
       if (connection != ToxConnection.NONE) {
         debug("is now connected to friend " + friendNumber)
-        addTask(new AliceBobTestBase.ChatClient.Task() {
-          def perform(tox: ToxCore): Unit = {
+        addTask(new Task() {
+          override def perform(tox: ToxCore): Unit = {
             val packet = ("_My name is " + getName).getBytes
             packet(0) = 160.toByte
             tox.sendLosslessPacket(friendNumber, packet)
