de76afcc9e21813fc0e4d428598b22aa4e3720a4
iphydf <iphydf@users.noreply.github.com>
2015-05-22 17:39:23 +0100

    Convert MemoryTest to scala.

---
+++ src/test/java/im/tox/tox4j/core/MemoryTest.scala
@@ -0,0 +1,26 @@
+package im.tox.tox4j.core
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+
+class MemoryTest extends JUnitSuite {
+  @Test
+  def testNoOpIterationConsumesNoMemory(): Unit = {
+    val values = ((5 to 15) map { iterations =>
+      ToxCoreFactory.withTox { tox =>
+        System.gc()
+        val memoryBefore = Runtime.getRuntime.totalMemory - Runtime.getRuntime.freeMemory
+        (0 to iterations * 10000) foreach { _ =>
+          tox.iteration()
+        }
+        val memoryAfter = Runtime.getRuntime.totalMemory - Runtime.getRuntime.freeMemory
+        (memoryAfter - memoryBefore) / iterations / 10000
+      }
+    }).sorted
+
+    val median = values(values.size / 2)
+    println(values)
+    assertEquals(0, median)
+  }
+}
