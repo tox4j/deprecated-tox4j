1d6ab643c17189657effe61db96f36853611f535
iphydf <iphydf@users.noreply.github.com>
2015-06-08 00:02:36 +0100

    Upload benchmark results regardless of success or failure.

--- src/test/java/im/tox/tox4j/core/bench/BenchWip.scala
+++ src/test/java/im/tox/tox4j/core/bench/BenchWip.scala
@@ -2,21 +2,35 @@ package im.tox.tox4j.core.bench
 
 import im.tox.tox4j.bench.PerformanceReportBase._
 import im.tox.tox4j.bench.{ Confidence, TimingReport }
-import im.tox.tox4j.core.ToxCore
+import im.tox.tox4j.core.{ ToxCoreFactory, ToxCore }
 
 /**
  * Work in progress benchmarks.
  */
 final class BenchWip extends TimingReport {
 
-  protected override def confidence = Confidence.high
+  protected override def confidence = Confidence.extreme
+
+  private def clearFriendList(pair: (Seq[Array[Byte]], ToxCore)): Unit = {
+    val tox = pair._2
+    tox.getFriendList foreach tox.deleteFriend
+  }
 
   timing of classOf[ToxCore] in {
 
-    measure method "iterationInterval" in {
-      usingTox(iterations10k) in {
-        case (sz, tox) =>
-          (0 until sz) foreach (_ => tox.iterationInterval)
+    measure method "addFriendNoRequest" in {
+      using(friends(100) map friendKeys, toxInstance.cached) setUp clearFriendList in {
+        case (friendList, tox) =>
+          friendList foreach tox.addFriendNoRequest
+      }
+      using(friends(100) map friendKeys, toxInstance) setUp clearFriendList in {
+        case (friendList, tox) =>
+          friendList foreach tox.addFriendNoRequest
+      }
+      using(friends(100) map friendKeys) in { friendList =>
+        ToxCoreFactory.withTox { tox =>
+          friendList foreach tox.addFriendNoRequest
+        }
       }
     }
 
@@ -27,7 +41,12 @@ final class BenchWip extends TimingReport {
    */
   object HoldingPen {
 
-    measure method "getFriendByPublicKey" in {
+    measure method "addFriend" in {
+      val message = Array.ofDim[Byte](1)
+      usingTox(friends1k map friendAddresses) setUp clearFriendList in {
+        case (friendList, tox) =>
+          friendList foreach (tox.addFriend(_, message))
+      }
     }
 
   }
