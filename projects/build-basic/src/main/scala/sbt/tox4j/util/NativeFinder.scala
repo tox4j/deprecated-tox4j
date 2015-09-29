f62237495acf8c5ed8461e43169eca3c0e206c8e
iphydf <iphydf@users.noreply.github.com>
2015-09-29 16:08:40 +0100

    Don't use null. Slightly limit scope of currentClassName.

--- projects/build-basic/src/main/scala/sbt/tox4j/util/NativeFinder.scala
+++ projects/build-basic/src/main/scala/sbt/tox4j/util/NativeFinder.scala
@@ -12,39 +12,45 @@ import sbt._
 
 object NativeFinder {
   def natives(classes: Set[File]): Map[String, Seq[String]] = {
-    var currentClassName: String = null
     var nativeList: Map[String, Seq[String]] = Map.empty
+
+    val handler = new MethodHandler {
+      var currentClassName: Option[String] = None
+
+      override def invoke(self: scala.Any, thisMethod: Method, proceed: Method, args: Array[AnyRef]) = {
+        thisMethod.getName match {
+          case "visit" =>
+            if (args.length > 2) {
+              val fileName = args(2).asInstanceOf[String]
+              currentClassName = Some(fileName.replaceAll("/", "."))
+            }
+          case "visitMethod" =>
+            val access = args(0).asInstanceOf[Int]
+            val name = args(1).asInstanceOf[String]
+            if ((access & Opcodes.ACC_NATIVE) != 0) {
+              currentClassName match {
+                case None => sys.error("Found method outside class")
+                case Some(enclosingClassName) =>
+                  nativeList.get(enclosingClassName) match {
+                    case None =>
+                      nativeList += ((enclosingClassName, Seq(name)))
+                    case Some(names) =>
+                      nativeList += ((enclosingClassName, name +: names))
+                  }
+              }
+            }
+          case methodName =>
+            sys.error(s"Unhandled method: $methodName")
+        }
+        null
+      }
+    }
 
     val factory = new ProxyFactory()
     factory.setSuperclass(classOf[ClassVisitor])
     factory.setFilter(new MethodFilter {
       override def isHandled(m: Method): Boolean = Seq("visit", "visitMethod").contains(m.getName)
     })
-
-    val handler = new MethodHandler {
-      override def invoke(self: scala.Any, thisMethod: Method, proceed: Method, args: Array[AnyRef]) = {
-        thisMethod.getName match {
-          case "visit" =>
-            if (args.length > 2) {
-              currentClassName = args(2).toString.replaceAll("/", ".")
-            }
-          case "visitMethod" =>
-            val access = args(0).asInstanceOf[Int]
-            val name = args(1).toString
-            if ((access & Opcodes.ACC_NATIVE) != 0) {
-              nativeList.get(currentClassName) match {
-                case None =>
-                  nativeList += ((currentClassName, Seq(name)))
-                case Some(names) =>
-                  nativeList += ((currentClassName, name +: names))
-              }
-
-            }
-          case _ =>
-        }
-        null
-      }
-    }
 
     factory.create(Array(classOf[Int]), Array(Integer.valueOf(Opcodes.ASM4)), handler) match {
       case x: ClassVisitor =>
