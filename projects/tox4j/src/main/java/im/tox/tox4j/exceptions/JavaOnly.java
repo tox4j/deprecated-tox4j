502c76443df61239ff43e6baa5e4966c912b02e5
iphydf <iphydf@users.noreply.github.com>
2015-10-06 16:42:30 +0100

    Add documentation for @JavaOnly.

--- projects/tox4j/src/main/java/im/tox/tox4j/exceptions/JavaOnly.java
+++ projects/tox4j/src/main/java/im/tox/tox4j/exceptions/JavaOnly.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark error codes in Java exception enums as Java-only, so they are not emitted as part of the error
+ * code conversion fragments in C++ (see {@link im.tox.tox4j.impl.jni.codegen.JniErrorCodes}).
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JavaOnly {
